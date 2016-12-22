3a6b809aeb23eaa4c672576e4558a95b51c55984
Wolke <wolkesau@gmail.com>
2016-12-22 16:29:05 +0800

    ok

--- src/index.tsx
+++ src/index.tsx
@@ -8,93 +8,128 @@ import { resolve } from 'universal-router';
 import * as express from 'express';
 
 
-class Hello extends React.Component<{title:string},{}>{
-  constructor(){
+class Hello extends React.Component<{ title: string }, {}>{
+  constructor() {
     super();
   }
-  render(){
-    return <h1>{this.props.title}</h1>
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.title}</h2>
+        
+        <a href="/contact" >contact</a>
+        <a href="/about" >about</a>
+        <a href="/" >home</a>
+
+
+      </div>)
   }
 };
 
-class App extends React.Component<{},{}>{
-  render(){
+class App extends React.Component<{}, {}>{
+  render() {
     return Children.only(this.props.children)
   }
 }
 
 const app = express();
 
-app.get("*",async (req,rep)=>{
+app.get("*", async (req, rep) => {
   var home = {
     path: '/',
     action() {
       let title = "home";
       return {
-        title ,
+        title,
         component: <Hello title={title} />,
       };
     },
   };
-   var contact = {
+  var contact = {
     path: '/contact',
     action() {
       let title = "contact";
       return {
-        title ,
+        title,
         component: <Hello title={title} />,
       };
     },
   };
- var r = {
-  path: '*',
-  action() {
+  var about = {
+    path: '/about',
+    action() {
+      let title = "about";
+      return {
+        title,
+        component: <Hello title={title} />,
+      };
+    },
+  };
+  var r = {
+    path: '*',
+    action() {
 
-    let title = "***";
+      let title = "***";
 
-    return {
-      title ,
-      component: <Hello title={title} />,
-    };
-  },
-};
+      return {
+        title,
+        component: <Hello title={title} />,
+      };
+    },
+  };
 
 
-const rs = {
-  path :"/",
-  children : [
-    home,
-    contact,
-    r
+  const rs = {
+    path: "/",
+    children: [
+      home,
+      contact,
+      about,
+      r
 
-  ],
-  async action({next}) {
-    const route = await next();
-    // console.log(route);
-    route.title =  `${route.title || 'Untitled Page'} - Great`;
-    route.description = route.description || '';
-    return route;
+    ],
+    async action({next}) {
+      const route = await next();
+      // console.log(route);
+      route.title = `${route.title || 'Untitled Page'} - Shit`;
+      route.description = route.description || '';
+      return route;
 
+    }
   }
-}
 
-function Html({title}){
-  return <div>{title}</div>
-}
+  function Html({title}) {
+    
+    return (
+      <div>
+        <h1>{title}</h1>
+        
+        <a href="/contact" >contact</a>
+        <br />
+        <a href="/about" >about</a>
+        <br />
+        
+        <a href="/" >home</a>
+
+
+      </div>)
+  }
+
   // const route = resolve( r , req.url);
-  const route = await resolve<any,any>( rs , {
-    path:req.url
+  const route = await resolve<any, any>(rs, {
+    path: req.url
   });
 
 
   const data = Object.assign(route);
-
+  console.log("data",data);
   const html = ReactDOMServer.renderToStaticMarkup(<Html {...data} />);
   rep.send(html);
 
 
 });
 
-app.listen(3030,()=>{
+app.listen(3030, () => {
   console.log("ok")
 });
